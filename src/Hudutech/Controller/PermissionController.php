e8fa29120c127030f664923e02bb6d31aebe96d4
njerucyrus <njerucyrus5@hotmail.com>
2017-04-08 12:40:52 +0300

    completed PermisionController implements

--- src/Hudutech/Controller/PermissionController.php
+++ src/Hudutech/Controller/PermissionController.php
@@ -10,38 +10,150 @@ namespace Hudutech\Controller;
 
 
 use Hudutech\AppInterface\PermissionInterface;
+use Hudutech\DBManager\DB;
 use Hudutech\Entity\Permission;
 
 class PermissionController implements PermissionInterface
 {
     public function create(Permission $permission)
     {
-        // TODO: Implement create() method.
+        $db = new DB();
+        $conn = $db->connect();
+        $permissionName = $permission->getPermissionName();
+        $permissionDescription = $permission->getPermissionDescription();
+        try {
+            $stmt = $conn->prepare("INSERT INTO permissions(perm_name, perm_description)
+                                    VALUES (:perm_name, :perm_description)");
+            $stmt->bindParam(":perm_name", $permissionName);
+            $stmt->bindParam(":perm_description", $permissionDescription);
+            $stmt->execute();
+            $db->closeConnection();
+            return true;
+        } catch (\PDOException $exception) {
+            echo $exception->getMessage();
+            return false;
+        }
+
     }
 
     public function update(Permission $permission, $id)
     {
-        // TODO: Implement update() method.
+        $db = new DB();
+        $conn = $db->connect();
+        $permissionName = $permission->getPermissionName();
+        $permissionDescription = $permission->getPermissionDescription();
+        try {
+            $stmt = $conn->prepare("UPDATE permissions SET perm_name=:perm_name,
+                                                           perm_description=:perm_description
+                                                        WHERE
+                                                             id=:id
+                                                            ");
+
+            $stmt->bindParam(":id", $id);
+            $stmt->bindParam(":perm_name", $permissionName);
+            $stmt->bindParam(":perm_description", $permissionDescription);
+            $stmt->execute();
+            $db->closeConnection();
+            return true;
+        } catch (\PDOException $exception) {
+            echo $exception->getMessage();
+            return false;
+        }
     }
 
     public static function getId($id)
     {
-        // TODO: Implement getId() method.
+        $db = new DB();
+        $conn = $db->connect();
+        try {
+            $stmt = $conn->prepare("SELECT * FROM permissions WHERE id=:id");
+            $stmt->bindParam(":id", $id);
+            $stmt->execute();
+
+            if ($stmt->rowCount() == 1) {
+                $row = $stmt->fetch(\PDO::FETCH_ASSOC);
+                $permission = array(
+                    "id" => $row['id'],
+                    "perm_name" => $row['perm_name'],
+                    "perm_description" => $row['perm_description']
+                );
+                $db->closeConnection();
+                return $permission;
+            } else {
+                return [];
+            }
+        } catch (\PDOException $exception) {
+
+            echo $exception->getMessage();
+            return [];
+        }
     }
 
     public static function delete($id)
     {
-        // TODO: Implement delete() method.
+        $db = new DB();
+        $conn = $db->connect();
+
+        try{
+            $stmt = $conn->prepare("DELETE FROM permissions WHERE id=:id");
+            $stmt->bindParam(":id",$id);
+            $stmt->execute();
+            $db->closeConnection();
+            return true;
+
+        } catch (\PDOException $exception) {
+            echo $exception->getMessage();
+            return false;
+
+        }
     }
 
     public static function destroy()
     {
-        // TODO: Implement destroy() method.
+        $db = new DB();
+        $conn = $db->connect();
+
+        try{
+            $stmt = $conn->prepare("DELETE FROM permissions");
+            $stmt->execute();
+            $db->closeConnection();
+            return true;
+
+        } catch (\PDOException $exception) {
+            echo $exception->getMessage();
+            return false;
+
+        }
     }
 
     public static function all()
     {
+        $db = new DB();
+        $conn = $db->connect();
+        try {
+            $stmt = $conn->prepare("SELECT * FROM permissions WHERE 1");
+            $stmt->execute();
 
+            if ($stmt->rowCount() > 0) {
+                $perms = array();
+               while ( $row = $stmt->fetch(\PDO::FETCH_ASSOC)) {
+                   $permission = array(
+                       "id" => $row['id'],
+                       "perm_name" => $row['perm_name'],
+                       "perm_description" => $row['perm_description']
+                   );
+                   $perms[] = $permission;
+               }
+                $db->closeConnection();
+                return $perms;
+            } else {
+                return [];
+            }
+        } catch (\PDOException $exception) {
+
+            echo $exception->getMessage();
+            return [];
+        }
     }
 
 }
