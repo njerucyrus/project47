eb56c8d7347ccb3a1caf7f92b6e6ca0a0da4a855
njerucyrus <njerucyrus5@hotmail.com>
2017-04-11 13:38:28 +0300

    completed exam crud

--- src/Hudutech/AppInterface/ExamInterface.php
+++ src/Hudutech/AppInterface/ExamInterface.php
@@ -16,6 +16,7 @@ interface ExamInterface
   public function create(Exam $exam);
   public function update(Exam $exam, $id);
   public static function delete($id);
+  public static function destroy();
   public static function getId($id);
   public static function getExams($term);
 
--- src/Hudutech/Controller/ExamController.php
+++ src/Hudutech/Controller/ExamController.php
@@ -10,40 +10,155 @@ namespace Hudutech\Controller;
 
 
 use Hudutech\AppInterface\ExamInterface;
+use Hudutech\DBManager\DB;
 use Hudutech\Entity\Exam;
 
 class ExamController implements ExamInterface
 {
     public function create(Exam $exam)
     {
+        $db = new DB();
+        $conn = $db->connect();
+
         $examName = $exam->getExamName();
         $outOf = $exam->getOutOf();
         $term = $exam->getTerm();
-
         try{
+            $stmt = $conn->prepare("INSERT INTO exams
+                                                    (
+                                                        term,
+                                                        exam_name,
+                                                        out_of
+                                                     ) 
+                                                VALUES
+                                                (
+                                                    :term,
+                                                    :exam_name,
+                                                    :out_of
+                                                )");
+            $stmt->bindParam(":term", $term);
+            $stmt->bindParam(":exam_name", $examName);
+            $stmt->bindParam(":out_of", $outOf);
+            $stmt->execute();
+            return true;
         } catch (\PDOException $exception){
             echo $exception->getMessage();
+            return false;
         }
     }
 
     public function update(Exam $exam, $id)
     {
-        // TODO: Implement update() method.
+        $db = new DB();
+        $conn = $db->connect();
+
+        $examName = $exam->getExamName();
+        $outOf = $exam->getOutOf();
+        $term = $exam->getTerm();
+        try{
+            $stmt = $conn->prepare("UPDATE exams SET exam_name=:exam_name, term=:term, out_of=:out_of WHERE id=:id");
+
+            $stmt->bindParam(":id", $id);
+            $stmt->bindParam(":term", $term);
+            $stmt->bindParam(":exam_name", $examName);
+            $stmt->bindParam(":out_of", $outOf);
+            $stmt->execute();
+            return true;
+        } catch (\PDOException $exception) {
+            echo $exception->getMessage();
+            return false;
+        }
+
     }
 
     public static function delete($id)
     {
-        // TODO: Implement delete() method.
+        $db = new DB();
+        $conn = $db->connect();
+        try{
+            $stmt = $conn->prepare("DELETE FROM exams WHERE id=:id");
+            $stmt->bindParam(":id",$id);
+            $stmt->execute();
+            return true;
+        }catch (\PDOException $exception){
+            echo $exception->getMessage();
+            return false;
+        }
     }
+
+    public static function destroy()
+    {
+        $db = new DB();
+        $conn = $db->connect();
+        try{
+            $stmt = $conn->prepare("DELETE FROM exams");
+            $stmt->execute();
+            return true;
+        }catch (\PDOException $exception){
+            echo $exception->getMessage();
+            return false;
+        }
+    }
+
 
     public static function getId($id)
     {
-        // TODO: Implement getId() method.
+        $db = new DB();
+        $conn = $db->connect();
+
+        try{
+            $stmt = $conn->prepare("SELECT * FROM exams WHERE id=:id");
+            $stmt->bindParam(":id",$id);
+            $stmt->execute();
+            if($stmt->rowCount() == 1) {
+                $row = $stmt->fetch(\PDO::FETCH_ASSOC);
+
+                $exam = array(
+                    "id" =>$row['id'],
+                    "exam_name" =>$row['exam_name'],
+                    "out_of"=>$row['out_of'],
+                    "term"=>$row['term']
+                    );
+
+                return $exam;
+            }
+            else{
+                return [];
+            }
+        }catch (\PDOException $exception){
+            echo $exception->getMessage();
+            return [];
+        }
     }
 
     public static function getExams($term)
     {
-        // TODO: Implement getExams() method.
+        $db = new DB();
+        $conn = $db->connect();
+
+        try{
+            $stmt = $conn->prepare("SELECT * FROM exams WHERE term=:term");
+            $stmt->bindParam(":term",$term);
+            $stmt->execute();
+            if($stmt->rowCount() == 1) {
+                $row = $stmt->fetch(\PDO::FETCH_ASSOC);
+
+                $exam = array(
+                    "id" =>$row['id'],
+                    "exam_name" =>$row['exam_name'],
+                    "out_of"=>$row['out_of'],
+                    "term"=>$row['term']
+                );
+
+                return $exam;
+            }
+            else{
+                return [];
+            }
+        }catch (\PDOException $exception){
+            echo $exception->getMessage();
+            return [];
+        }
     }
 
 }
